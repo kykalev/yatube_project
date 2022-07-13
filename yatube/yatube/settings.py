4181e0d686ac33e92a9c2467b2df0a26ed6bee05
Кукалев Максим <m.kykalev@yandex.ru>
2022-07-13 21:13:25 +0300

    Удалил приложение post

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'post.app.PostConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
