8f0664dcf3905bbd25025a42dcfd6a37dce2316b
Кукалев Максим <m.kykalev@yandex.ru>
2022-07-16 18:15:09 +0300

    Подключение шаблона

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
+from re import template
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Главная страница социальной сети Yatube')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
 def group_posts(request, slug):
