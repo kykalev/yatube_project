58c33e5febe8f1e57bd7b4cd337e95adccf8810a
Кукалев Максим <m.kykalev@yandex.ru>
2022-07-17 11:29:51 +0300

    Подключено статическое оформление css и img

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,9 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
